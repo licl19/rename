62923dd50896c825ab4c7998e30f889f12e3488e
lichanglai <licl19@163.com>
2018-03-23 02:25:31 +0800

    修改find方法

--- rename.py
+++ rename.py
@@ -4,10 +4,10 @@ path = './'
 
 for file in os.listdir(path):
     if os.path.isfile(os.path.join(path,file))==True:
-    	if file.find('.') == 0:
+    	if file.find('.') != -1:
             newnamelist = file.split('.')
             print newnamelist[0]
             print newnamelist[1]
-            if newnamelist[1].find('png') == 0:
+            if newnamelist[1].find('png') != -1:
          	     newname = newnamelist[0] + '.jpg'
          	     os.rename(os.path.join(path,file),os.path.join(path,newname))
