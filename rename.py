70e7f92828161949f3e13cf75c0645995441b5bb
lichanglai <licl19@163.com>
2018-03-23 02:20:55 +0800

    修改python文件

--- rename.py
+++ rename.py
@@ -4,9 +4,10 @@ path = './'
 
 for file in os.listdir(path):
     if os.path.isfile(os.path.join(path,file))==True:
-        newnamelist = file.split('.')
-        print newnamelist[0]
-        print newnamelist[1]
-        if newnamelist[1].find('py') == -1:
-         	newname = newnamelist[0] + '.jpg'
-         	os.rename(os.path.join(path,file),os.path.join(path,newname))
+    	if file.find('.') == 0:
+            newnamelist = file.split('.')
+            print newnamelist[0]
+            print newnamelist[1]
+            if newnamelist[1].find('png') == 0:
+         	     newname = newnamelist[0] + '.jpg'
+         	     os.rename(os.path.join(path,file),os.path.join(path,newname))
